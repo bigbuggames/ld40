9644f4c3d67d06258ded3af79a3e354cd516512f
Marc Solà Nadal <marcsolanadal@gmail.com>
2017-12-04 19:29:34 +0100

    Added levels to the game

--- src/constants/levels.js
+++ src/constants/levels.js
@@ -1,30 +1,65 @@
 const levels = [
   {
     required: ['intro'],
-    exactWords: 'Hello traveler',
-    solution: 'hello',
-    prep: 'prep-meat',
+    exactWords: 'Salutations chef',
+    solution: 'salutationpen ramsay',
+    prep: 'idle-happy',
     notes: {
       grammar: [
-        'words ending with (fu) now end with (mu)'  
+        'words ending with (s) now end with (pen)'  
       ],
       dictionary: [
-        'hello => loler'
+        'chef => ramsay'
       ]
     }
   },
   {
     required: ['intro'],
-    exactWords: 'I want some rice',
-    solution: 'oh que poya',
-    prep: 'prep-rice',
+    exactWords: 'Martian pinapples ala Pikotaro',
+    solution: 'portian pinapplepen ala pikotaro',
+    prep: 'prep-veggies',
     notes: {
       grammar: [
-        'sefsefsffgesg'
+        '(ma, me, mo) now are (po)'
+      ],
+      dictionary: []
+    }
+  },
+  {
+    required: ['intro'],
+    exactWords: 'Homemade soup of the Gods',
+    solution: 'pep godpen hopopode of',
+    prep: 'prep-soup',
+    notes: {
+      grammar: [
+        'everything before (the) it\'s placed at the end of the sentence'
       ],
       dictionary: [
-        'oh => que',
-        'rice => poya'
+        'the => pep'
+      ]
+    }
+  },
+  {
+    required: ['intro'],
+    exactWords: 'The mother of all marmalade mountains',
+    solution: 'pother of lla porpolade pountainpen pep',
+    prep: 'prep-dessert',
+    notes: {
+      grammar: [
+        'words starting with (a) get turned around'
+      ],
+      dictionary: []
+    }
+  },
+  {
+    required: ['intro'],
+    exactWords: 'Give me the bill please',
+    solution: 'pep petrifax please give po',
+    prep: 'petrifax',
+    notes: {
+      grammar: [],
+      dictionary: [
+        'bill => petrifax' 
       ]
     }
   }
