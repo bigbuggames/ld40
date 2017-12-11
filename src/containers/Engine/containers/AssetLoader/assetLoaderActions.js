baf560b4d5ebcb1f02fab1b1d45db3fd570b2181
Marc Solà Nadal <marcsolanadal@gmail.com>
2017-12-11 22:47:17 +0100

    Removed hardcoded assetUrls from the project

--- src/containers/Engine/containers/AssetLoader/assetLoaderActions.js
+++ src/containers/Engine/containers/AssetLoader/assetLoaderActions.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-console */
-
 import axios from 'axios'
-// import localForage from 'localforage'
 
 import { 
   LOAD_ASSETS_PENDING,
@@ -10,16 +7,17 @@ import {
   ASSET_LOADED
 } from './actionTypes'
 
-import assetUrls from 'globals/assetUrls'
-
-let assets = assetUrls;
-if (process.env.NODE_ENV !== 'production') {
-  assets = assetUrls.map((asset) => { 
-    return {
-      ...asset, 
-      url: `https://cors-anywhere.herokuapp.com/${asset.url}`
-    }
-  })
+const getAssetUrls = () => {
+  return axios.get('https://ld40-703cd.firebaseio.com/.json')
+    .then((res) => {
+      if (process.env.NODE_ENV !== 'production') {
+        return res.data.map((asset) => ({ 
+          ...asset, 
+          url: `https://cors-anywhere.herokuapp.com/${asset.url}`
+        }))
+      }
+      return res.data
+    })
 }
 
 const loadAsset = (asset, dispatch) => {
@@ -37,19 +35,20 @@ const loadAsset = (asset, dispatch) => {
         blobUrl: URL.createObjectURL(file.data)
       }
     })
-    // return localForage.setItem(asset.name, file.data)
   })
-  .catch((err) => console.error(err))
 }
 
 export const loadAssets = () => {
   return (dispatch) => {
     dispatch({ type: LOAD_ASSETS_PENDING })
 
-    axios
-      .all(assets.map((asset) => loadAsset(asset, dispatch)))
+    getAssetUrls()
+      .then((assets) => {
+        return axios.all(assets.map((asset) => loadAsset(asset, dispatch)))
+      })
       .then((res) => dispatch({ type: LOAD_ASSETS_SUCCESS }))
       .catch((err) => dispatch({ type: LOAD_ASSETS_ERROR }))
+
   }
 }
 
